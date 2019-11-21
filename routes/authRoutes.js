53543e1bf0586a452014256d985fe8f3c19f5787
Phi Nguyen <pnguy55@gmail.com>
2019-11-21 13:51:09 -0600

    set up express server and basic apassport auth

---
+++ routes/authRoutes.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+
+module.exports = app => {
+    // All routes wrapped in a function
+    app.get(
+        '/auth/google', 
+        passport.authenticate('google', {
+            // asks google for predetermined objects
+            scope: ['profile', 'email']
+        })
+    );
+
+    // After user approves of google auth, this is where google goes 
+    app.get('/auth/google/callback', passport.authenticate('google'));
+    
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
+
+    app.get('/api/logout', (req, res) => {
+        // passport auto included function
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/' , (req, res) => {
+        res.send(req.user);
+    })
+    // All routes wrapped in a function
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import express from 'express';
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+require('../models/User');
+require('../services/passport');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_KEY]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('../routes/authRoutes')(app);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server is up on port ' + PORT)
+});
