17f695f9efef010fc32e24fb8caccc58583facd8
Phi Nguyen <pnguy55@gmail.com>
2020-01-03 14:37:46 -0600

    Add welcome emails

--- services/sendgridMailer.js
+++ services/sendgridMailer.js
@@ -10,15 +10,20 @@ const sendWelcomeEmail = (email, firstName) => {
         subject: 'Thanks for using My Video Tagger! - CodePhony',
         html: `
         <h5>Hi ${firstName}!</h5>
+
         <p>
             Thank you so much for using My Video Tagger, please remember that you can always go to 
             your dashboard to look at tag lists you've already made :)
-
+        </p>
+        <p>
             Best wishes!
             Code Phony
-
+        </p>
+        <p>
             PS: If you found my tool helpful, plz consider subscribing to my Youtube Channel for more valuable content
-            <a href='https://www.youtube.com/channel/UCIw9p-0zI1rEPEs_SS6fDkg?sub_confirmation=1' >Subscribe to CodePhony</a>
+        </p>
+        <p> 
+           <a href='https://www.youtube.com/channel/UCIw9p-0zI1rEPEs_SS6fDkg?sub_confirmation=1' >Subscribe to CodePhony</a>
         </p>
         `
     })
