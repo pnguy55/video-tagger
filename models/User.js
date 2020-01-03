108f4bc58fa8cda2afd9fc31a94f8b258f2afd78
Phi Nguyen <pnguy55@gmail.com>
2020-01-03 12:40:27 -0600

    Add email to profiles

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     googleId: String,
+    email: String,
     yes: false,
     no: false
 });
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const User = mongoose.model('users');
 
@@ -24,12 +25,14 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
+      const email =_.map( profile.emails, ({ value }) =>{ return value});
 
+      console.log(email[0])
       if (existingUser) {
         return done(null, existingUser);
       }
-
-      const user = await new User({ googleId: profile.id }).save();
+      
+      const user = await new User({ googleId: profile.id, email: email[0] }).save();
       done(null, user);
     }
   )
